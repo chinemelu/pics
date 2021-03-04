7e4a4275948b1c72cbcc1dcb3a10f5b7cea15c82
Chinemelu Nwosu <chinemelunwosu@gmail.com>
2021-03-04 23:30:13 +0100

    convert app to class component and pass function as prop

---
+++ src/components/SearchBar.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+  state = {
+    term: '',
+  };
+  onFormSubmit = (event) => {
+    event.preventDefault();
+    this.props.onSubmit((this.state.term))
+  }
+
+  render() {
+    return (
+      <div className="ui segment">
+        <form className="ui form" onSubmit={this.onFormSubmit}>
+          <div className="field">
+            <label htmlFor="term">Image Search</label>
+            <input
+              type="text"
+              id="term"
+              value={this.state.term}
+              onChange={(e) => this.setState({ term: e.target.value })}
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+SearchBar.propTypes = {
+  onSubmit: Function
+}
+
+export default SearchBar;
