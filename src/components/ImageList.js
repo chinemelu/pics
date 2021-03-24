60a630a8785868cdc77062e74289c20fecaf74eb
Chinemelu Nwosu <chinemelunwosu@gmail.com>
2021-03-24 23:25:39 +0100

    Create ImageList component and add css file to it

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
+import './ImageList.css'
 const ImageList = (props) => {
-  const images = props.images.map(image => {
-    return <img src={image.urls.regular} key={image.id} />;
+  const images = props.images.map(({ description, id, urls }) => {
+    return <img className="image" src={urls.regular} key={id} alt={description} />;
   })
-  console.log(images);
-  return <div>{images}</div>
+  return <div className="image-list">{images}</div>
 }
   
 ImageList.propTypes = {
